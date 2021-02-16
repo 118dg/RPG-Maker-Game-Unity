4256e7718f8c4cc8ad46a1f5ddc0284a6a8f8c24
118dg <118dg@naver.com>
2021-02-16 23:55:09 +0900

    part 9 - 진행중

---
+++ RPG MAKER GAME/Assets/Scripts/BGMManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    static public BGMManager instance;
+
+    public AudioClip[] clips; // 배경음악들
+    
+    private AudioSource source;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void Play()
+    {
+
+    }
+
+    public void Stop()
+    {
+
+    }
+
+    public void FadeOutMusic()
+    {
+
+    }
+}
